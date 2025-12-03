931c0c50cd89d4fb2fc95065234de612b66f51cc
Tathya Dixit <dixit.tathya2003@gmail.com>
2025-12-03 12:16:25 +0530

    add : validators, constraints and helper methods

--- recipes/models.py
+++ recipes/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.conf import settings
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+
 
 class Recipe(models.Model):
     title = models.CharField(max_length = 100)
@@ -14,12 +17,26 @@ class Recipe(models.Model):
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
 
+    def average_rating(self):
+        reviews = self.reviews.all()
+        if reviews.exists():
+            return round((sum(r.rating for r in reviews) / reviews.count()), 1)
+        return 0
+    
+    def total_reviews(self):
+        return self.reviews.count()
+
     def __str__(self):
         return f"{self.title} - {self.author.username}" 
+    
+    class Meta:
+        ordering = ['-created_at']
+
+
 
 class Review(models.Model):
     review = models.TextField(max_length=500)
-    rating = models.IntegerField(default=0)
+    rating = models.IntegerField(default=0, validators = [MinValueValidator(1), MaxValueValidator(5)], help_text="rate from 1 - 5 stars")
     
     recipe = models.ForeignKey(Recipe, on_delete = models.CASCADE, related_name = 'reviews')
     reviewer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE, related_name = 'reviews')
@@ -29,6 +46,10 @@ class Review(models.Model):
 
     def __str__(self):
         return f"{self.reviewer.username} - {self.recipe.title} : {self.rating}"
+    
+    class Meta:
+        unique_together = ['recipe', 'reviewer']
+        ordering = ['-created_at']
 
 
 
