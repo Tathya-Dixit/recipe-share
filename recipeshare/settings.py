eebac7e0a3a0999aecdd6a26ef620ca1ff27eeb1
Tathya Dixit <dixit.tathya2003@gmail.com>
2025-12-27 11:41:18 +0530

    fix : minor bugs

--- recipeshare/settings.py
+++ recipeshare/settings.py
@@ -24,9 +24,9 @@ load_dotenv()
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', []).split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',') if os.environ.get('ALLOWED_HOSTS') else []
 
 # Application definition
 
